588d8b4419941e3f9a59e6ba60e1a5f2dc4f680c
Githmin Jayawardhana <githmin21@gmail.com>
2022-11-20 19:10:32 +0530

    added sign up  page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Signin from "./components/Signin";
-import styled from "styled-components";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Register />} />
       </Routes>
     </div>
   );
